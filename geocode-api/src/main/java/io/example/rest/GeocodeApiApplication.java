da58339a5ce21ade6f0a025dafd1e3a2573e866c
SonaliMathwaniya999 <sonalimathwaniya.git.99@gmail.com>
2023-03-06 17:18:44 +0530

    city distance calculator app.

---
+++ geocode-api/src/main/java/io/example/rest/GeocodeApiApplication.java
@@ -0,0 +1,13 @@
+package io.example.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeocodeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeocodeApiApplication.class, args);
+	}
+
+}
